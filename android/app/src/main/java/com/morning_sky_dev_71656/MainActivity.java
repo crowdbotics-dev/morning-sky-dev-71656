3566321a94e7aca3c79a35f27a3b515b5201ec17
Crowdbotics <engineering@crowdbotics.com>
2023-03-24 00:18:55 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/morning_sky_dev_71656/MainActivity.java
@@ -0,0 +1,15 @@
+package com.morning_sky_dev_71656;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "morning_sky_dev_71656";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "morning-sky-dev-71656",
+  url: "https://morning-sky-dev-71656.botics.co",
+  api: "https://morning-sky-dev-71656.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
